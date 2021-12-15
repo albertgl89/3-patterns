0a8bfb8f2694724b351133bd64cd98274d3dde0a
Albert Garcia Llorca <albertgarcia20@gmail.com>
2021-12-15 14:56:14 +0000

    Nivell 2 / Enunciat

---
+++ nivell-2/poultry.php
@@ -0,0 +1,38 @@
+<?php
+
+//Voleu que els seus galls dindis es comportin com ànecs, de manera que ha d'aplicar el adapter pattern.
+//En el mateix arxiu, escriviu una classe anomenada TurkeyAdapter i assegureu-vos que tingui en compte el següent:
+
+//La traducció de l'quack entre classes és fàcil: simplement cridi al mètode Gobble quan sigui apropiat.
+
+//Encara que ambdues classes tenen un mètode fly, els galls dindis només poden volar a ratxes curtes,
+// no poden volar llargues distàncies com els ànecs. Per mapejar entre el mètode fly d'un ànec i el mètode de l'gall dindi, 
+// ha de trucar a l'mètode fly de el gall dindi cinc vegades per compensar-ho.
+
+class Duck
+{
+
+    public function quack()
+    {
+        echo "Quack \n";
+    }
+
+    public function fly()
+    {
+        echo "I'm flying \n";
+    }
+}
+
+class Turkey
+{
+
+    public function gobble()
+    {
+        echo "Gobble gobble \n";
+    }
+
+    public function fly()
+    {
+        echo "I'm flying a short distance \n";
+    }
+}
